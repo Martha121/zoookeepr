d6749e33e2c4b8c5b4c635756e6bbace29de890c
MGcommits <marthagamez211@gmail.com>
2021-02-15 16:19:18 -0600

    Add request por single animal

--- server.js
+++ server.js
@@ -45,11 +45,24 @@ if (query.name) {
 // return the filtered results:
 return filteredResults;
 }
-
-app.get('/api/animals', (req, res) => {
-let results = animals;
-if (req.query) {
-    results = filterByQuery(req.query, results);
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
   }
-res.json(results);
-});
+
+  app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
+  });
