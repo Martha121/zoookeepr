e0a8e79479a653049c9633e92674d7240c7e43ab
MGcommits <marthagamez211@gmail.com>
2021-02-23 10:33:39 -0600

    Create Zookeepers end points

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const animalRoutes = require('../apiRoutes/animalRoutes');
+
+router.use(animalRoutes);
+
+router.use(require('./zookeepersRoutes'));
+
+module.exports = router;
